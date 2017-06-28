a16c5421571079ebac79fff9644f473be64f7079
Angad <angads25@gmail.com>
2017-06-28 17:45:31 +0530

    feat: Boiler Plate Code

---
+++ stargazer/src/main/java/com/github/angads25/stargazer/StargazerView.java
@@ -0,0 +1,46 @@
+package com.github.angads25.stargazer;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * <p>
+ * Created by Angad on 28-06-2017.
+ * </p>
+ */
+
+public class StargazerView extends View {
+    public StargazerView(Context context) {
+        super(context);
+    }
+
+    public StargazerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StargazerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public StargazerView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void initView() {
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
