32a7bee75189494499b3726e254429a4efc1fe57
Angad <angads25@gmail.com>
2017-06-28 18:17:25 +0530

    feat: Added support variables

--- stargazer/src/main/java/com/github/angads25/stargazer/StargazerView.java
+++ stargazer/src/main/java/com/github/angads25/stargazer/StargazerView.java
@@ -3,8 +3,10 @@ package com.github.angads25.stargazer;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -14,24 +16,39 @@ import android.view.View;
  */
 
 public class StargazerView extends View {
+    private Paint paint;
+    private int radius;
+    private int centerX;
+    private int centerY;
+
+    private long downTime;
+    private float downX;
+    private float downY;
+
     public StargazerView(Context context) {
         super(context);
+        initView();
     }
 
     public StargazerView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initView();
     }
 
     public StargazerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView();
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public StargazerView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        initView();
     }
 
     private void initView() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
     }
 
     @Override
@@ -42,5 +59,30 @@ public class StargazerView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        radius = Math.min(width, height);
+        setMeasuredDimension(radius, radius);
+        radius = radius >>> 1;
+        centerX = (getRight() - getLeft()) >>> 1;
+        centerY = (getBottom() - getTop()) >>> 1;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:   downTime = System.currentTimeMillis();
+                                            downX = x; downY = y;
+                                            break;
+
+            case MotionEvent.ACTION_UP: long diff = System.currentTimeMillis() - downTime;
+                                        if(diff < 200) {
+                                            //TODO: Consider Touch Case
+                                        }
+                                        break;
+        }
+        return super.onTouchEvent(event);
     }
 }
